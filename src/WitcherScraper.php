87ceb97f696fe2f89b4eebfad543903d01b3311c
Charles <charleslambretpro@gmail.com>
2023-07-03 16:51:12 +0200

    filtrage des contenus, sauvegarde des données à chaque scrap, user friendliness, id des objets

--- src/WitcherScraper.php
+++ src/WitcherScraper.php
@@ -16,24 +16,23 @@ class WitcherScraper
 
     public function getCharacters()
     {
-        $res = $this->client->request('GET', 'https://witcher-archive.fandom.com/fr/wiki/Cat%C3%A9gorie:Personnages_dans_The_Witcher');
-
-        $crawler = new Crawler((string) $res->getBody(), 'https://witcher-archive.fandom.com');
-
         $characters = [];
-        $crawler->filter('.category-page__member-link')->each(function (Crawler $node, $i) use (&$characters) {
+        $id = 0;
+        $res = $this->client->request('GET', 'https://witcher-archive.fandom.com/fr/wiki/Cat%C3%A9gorie:Personnages_dans_The_Witcher');
+        $crawler = new Crawler((string) $res->getBody(), 'https://witcher-archive.fandom.com');
+        $crawler->filter('.category-page__member-link')->each(function (Crawler $node, $i) use (&$characters, &$id) {
             $characterName = $node->text();
             echo "Récupération des infos du personnage : " . $characterName . "\n";
 
             $characterLink = $node->link()->getUri();
             $res = $this->client->request('GET', $characterLink);
-            $crawler = new Crawler((string) $res->getBody(), 'https://witcher-archive.fandom.com');
+            $characterCrawler = new Crawler((string) $res->getBody(), 'https://witcher-archive.fandom.com');
             
-            $description = $crawler->filter('.mw-parser-output p')->slice(0, 3)->each(function (Crawler $node, $i) {
+            $description = $characterCrawler->filter('.mw-parser-output p')->slice(0, 3)->each(function (Crawler $node, $i) {
                 return $node->text();
             });
 
-            $associatedContents = $crawler->filter('.mw-parser-output p a')->each(function (Crawler $node, $i) {
+            $associatedContents = $characterCrawler->filter('.mw-parser-output p a')->each(function (Crawler $node, $i) {
                 $content = new \stdClass();
                 $content->text = $node->text();
                 $content->href = $node->attr('href');
@@ -42,6 +41,7 @@ class WitcherScraper
 
             // Create a new object and set its properties
             $character = new \stdClass();
+            $character->id = $id++;
             $character->name = $characterName;
             $character->description = $description;
             $character->associatedContents = $associatedContents;
@@ -53,29 +53,68 @@ class WitcherScraper
     }
 
     public function getAssociatedContents($characters)
-{
-    $contents = [];
-    foreach ($characters as $character) {
-        foreach ($character->associatedContents as $associatedContent) {
-            echo "Récupération du contenu associé : " . $associatedContent->text . "\n";
+    {
+        $contents = [];
+        $fetchedUrls = [];
+        $count = 0;
+        $id = 0;
+        foreach ($characters as $character) {
+            foreach ($character->associatedContents as $associatedContent) {
+                if ($count >= 200) {
+                    break 2;
+                }
 
-            $contentLink = 'https://witcher-archive.fandom.com' . $associatedContent->href;
-            $res = $this->client->request('GET', $contentLink);
-            $crawler = new Crawler((string) $res->getBody(), 'https://witcher-archive.fandom.com');
+                $contentLink = $associatedContent->href;
+                if (!preg_match('/^https?:\/\//', $contentLink)) {
+                    $contentLink = 'https://witcher-archive.fandom.com' . $contentLink;
+                }
 
-            $descriptionNode = $crawler->filter('.mw-parser-output p')->first();
-            $description = $descriptionNode->count() > 0 ? $descriptionNode->text() : '';
+                if (isset($fetchedUrls[$contentLink])) {
+                    continue;
+                }
 
-            // Create a new object and set its properties
-            $content = new \stdClass();
-            $content->name = $associatedContent->text;
-            $content->description = $description;
+                echo "Récupération du contenu associé : " . $associatedContent->text . "\n";
 
-            $contents[] = $content;
+                try {
+                    $res = $this->client->request('GET', $contentLink);
+                    $crawler = new Crawler((string) $res->getBody(), 'https://witcher-archive.fandom.com');
+
+                    $descriptionNode = $crawler->filter('.mw-parser-output p')->first();
+                    $description = $descriptionNode->count() > 0 ? $descriptionNode->text() : '';
+
+                    $content = new \stdClass();
+                    $content->id = $id++;
+                    $content->characterId = $character->id;
+                    $content->name = $associatedContent->text;
+                    $content->description = $description;
+
+                    $contents[] = $content;
+                    $fetchedUrls[$contentLink] = true;
+                    $count++;
+                } catch (\Exception $e) {
+                    echo "Une erreur s'est produite lors de la récupération du contenu associé : " . $e->getMessage() . "\n";
+                }
+            }
         }
+
+        return $contents;
     }
 
-    return $contents;
-}
+    public function saveData($characters, $contents)
+    {
+        file_put_contents(__DIR__ . '/data/characters.json', json_encode($characters));
+        file_put_contents(__DIR__ . '/data/contents.json', json_encode($contents));
+    }
 
+    public function getSavedCharacters()
+    {
+        $data = file_get_contents(__DIR__ . '/data/characters.json');
+        return json_decode($data);
+    }
+
+    public function getSavedContents()
+    {
+        $data = file_get_contents(__DIR__ . '/data/contents.json');
+        return json_decode($data);
+    }
 }
--- src/index.php
+++ src/index.php
@@ -5,14 +5,35 @@ use Charleslambret\Witchercharacterscrapping\WitcherScraper;
 
 $scraper = new WitcherScraper();
 
-echo "Début du scrapping des personnages...\n";
-$characters = $scraper->getCharacters();
+// Check if data files already exist
+if (file_exists(__DIR__ . '/data/characters.json') && file_exists(__DIR__ . '/data/contents.json')) {
+    echo "Un jeu de données est déjà sauvegardé dans le package. Voulez-vous le mettre à jour ? (y/n) ";
+    $response = trim(fgets(STDIN));
 
-echo "Début du scrapping des contenus associés...\n";
-$contents = $scraper->getAssociatedContents($characters);
+    if (strtolower($response) === 'y') {
+        echo "Début du scrapping des personnages...\n";
+        $characters = $scraper->getCharacters();
 
-echo "Scrapping terminé. Voici les contenus associés :\n";
+        echo "Début du scrapping des contenus associés...\n";
+        $contents = $scraper->getAssociatedContents($characters);
+
+        // Save the scraped data
+        $scraper->saveData($characters, $contents);
+
+        echo "Scrapping terminé. Sauvegarde des données...\n";
+    }
+}
+
+// Retrieve the saved data
+$characters = $scraper->getSavedCharacters();
+$contents = $scraper->getSavedContents();
+
+echo "Voici les personnages :\n";
+foreach ($characters as $character) {
+    print_r($character);
+}
+
+echo "Voici les contenus associés :\n";
 foreach ($contents as $content) {
     print_r($content);
 }
-
